43f72a9438a0aab5846858fc5e9d4e04d3bbd0c7
Pragati Khettry <pkhettry@in-mac-43.corp.yodlee.com>
2019-06-09 22:59:36 +0530

    readme commit

---
+++ server/app.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const errorHandler = require('errorhandler');
+const mongoose = require('mongoose');
+
+mongoose.promise = global.Promise;
+
+// const isProduction = process.env.NODE_ENV === 'production';
+
+const app = express();
+
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'Library', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+// if(!isProduction) {
+//   app.use(errorHandler());
+// }
+app.use(errorHandler());
+mongoose.connect("mongodb://localhost/library",function(err){
+     if(err) console.log(err);
+});
+mongoose.set('debug', true);
+
+require('./models/Books');
+app.use(require('./routes'));
+
+// app.use((req, res, next) => {
+//   const err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+// if (!isProduction) {
+//   app.use((err, req, res) => {
+//     res.status(err.status || 500);
+
+//     res.json({
+//       errors: {
+//         message: err.message,
+//         error: err,
+//       },
+//     });
+//   });
+// }
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
+});
+
+app.listen(8000, () => console.log('Server started on http://localhost:8000'));
